10ac560445515c4cc5cbcad754c74bb9e63682d8
Alan Gonzalez <neunapp.goba080428hhgnrla0@soycecytem.mx>
2026-01-19 12:14:27 -0600

    Actu6

--- CecyRe/cecytemrecicla/Settings/base.py
+++ CecyRe/cecytemrecicla/Settings/base.py
@@ -38,7 +38,7 @@ ROOT_URLCONF = 'cecytemrecicla.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR.parent / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
